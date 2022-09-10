ea7980cb6b88f3e1c1e2dba7d2504bd1376b772f
djumanov <diyorbekjumanov1120@gmail.com>
2022-09-10 21:29:24 +0500

    welcome button

--- commands.py
+++ commands.py
@@ -1,8 +1,21 @@
-from telegram import Update
+from telegram import ReplyKeyboardMarkup, Update, KeyboardButton
 from telegram.ext import CallbackContext
+
+
+
+def keyboard() -> list:
+    """create welocome buttons."""
+    keyboard = [
+        [KeyboardButton(text='🏬 Catalog'), KeyboardButton(text='📦 Orders')],
+        [KeyboardButton(text='👤 Userinfo'), KeyboardButton(text='🛒 Cart')],
+        [KeyboardButton(text='🎛 Administration')],
+    ]
+    return keyboard
+
+
 
 def start(update: Update, context: CallbackContext) -> None:
     """Send a message when the command /start is issued."""
-    update.message.reply_text(text='Hello! 👋 \nThis is a demo version of the Telegram Store bot. You can test out catalog function and checkout process.')
+    text = 'Hello! 👋 \nThis is a demo version of the Telegram Store bot. You can test out catalog function and checkout process.'
+    update.message.reply_text(text=text, reply_markup=ReplyKeyboardMarkup(keyboard=keyboard(), resize_keyboard=True))
 
-    
