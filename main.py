018a601fc8dcda67d5bc803f991cb70f3889dd03
djumanov <diyorbekjumanov1120@gmail.com>
2022-09-19 16:15:26 +0500

    adminstration and exit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import os
 from commands import start
+from services import administration, administration_exit
 
 
 def main() -> None:
@@ -11,6 +12,8 @@ def main() -> None:
     dispatcher = updater.dispatcher
 
     dispatcher.add_handler(handler=CommandHandler(command=['start', 'boshlash'], callback=start))
+    dispatcher.add_handler(handler=MessageHandler(filters=Filters.text('🎛 Administration'), callback=administration))
+    dispatcher.add_handler(handler=MessageHandler(filters=Filters.text('🚪 Exit'), callback=administration_exit))
 
     updater.start_polling()
     updater.idle()
